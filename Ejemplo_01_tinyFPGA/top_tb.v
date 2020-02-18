c8df08f414d3dd0a96754853bc3783434069a660
kekellner <kekellner@uvg.edu.gt>
2020-02-18 11:16:58 -0600

    Changed display format

--- Ejemplo_01_tinyFPGA/top_tb.v
+++ Ejemplo_01_tinyFPGA/top_tb.v
@@ -8,8 +8,8 @@ wire out_0;
 top ejemplo (in_1, in_0, out_0);
 
   initial begin
-    $display("In 1/t In 2 /t Out 1");
-    $monitor("%b  /t %b  /t %b",in_1, in_0, out_0);
+    $display("In 1\t In 2 \t Out 1");
+    $monitor("%b  \t %b  \t %b",in_1, in_0, out_0);
     in_1 = 0;
     in_0 = 0;
     #1 in_1 = 1;
