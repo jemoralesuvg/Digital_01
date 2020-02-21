8419a8cf8115216fa29ae2b777157dbfb2dead37
kekellner <kekellner@uvg.edu.gt>
2020-02-21 09:54:35 -0600

    Fixed l-value error in seven_seg using an internal variable set as reg

--- seven_seg/seven_seg.v
+++ seven_seg/seven_seg.v
@@ -1,21 +1,23 @@
 module seven_seg (input wire [3:0] data, output [6:0] segments);
 
-  wire [6:0] segments;
+  reg [6:0] segments_reg;
 
   always @ (*)  begin
     case (data)
-      0: segments = 7'b111_1110;
-      1: segments = 7'b011_0000;
-      2: segments = 7'b110_1101;
-      3: segments = 7'b111_1001;
-      4: segments = 7'b011_0011;
-      5: segments = 7'b101_1011;
-      6: segments = 7'b101_1111;
-      7: segments = 7'b111_0000;
-      8: segments = 7'b111_1111;
-      9: segments = 7'b111_0011;
-      default: segments = 7'b000_0000;
+      0: segments_reg = 7'b111_1110;
+      1: segments_reg = 7'b011_0000;
+      2: segments_reg = 7'b110_1101;
+      3: segments_reg = 7'b111_1001;
+      4: segments_reg = 7'b011_0011;
+      5: segments_reg = 7'b101_1011;
+      6: segments_reg = 7'b101_1111;
+      7: segments_reg = 7'b111_0000;
+      8: segments_reg = 7'b111_1111;
+      9: segments_reg = 7'b111_0011;
+      default: segments_reg = 7'b000_0000;
     endcase
   end
+
+  assign segments = segments_reg;
 
 endmodule
