fb460fae8ca51466188bc1c38daf5e31b62312c2
kekellner <kekellner@uvg.edu.gt>
2020-02-25 03:08:40 -0600

    Modified full adder to include an "always" example

--- full_adder/full_adder.v
+++ full_adder/full_adder.v
@@ -9,3 +9,34 @@ module full_adder(input a, b, cin, output s, cout);
   assign cout = g | (p & cin);
 
 endmodule
+
+
+/*
+module full_adder(input a, b, cin, output reg s, cout);
+
+  reg p, g;
+
+  // Los bloques always sólo pueden manejar reg
+  // Las salidas s y cout tienen que ser reg porque los estamos manipulando
+  // dentro del always
+  // Otra opción sería hacer variables intermedias y luego assigns
+  // Realmente es mejor dejar las entradas y salidas como nets
+
+  //reg sReg, coutReg;
+
+  always @ (a, b, cin) begin
+    p = a ^ b;
+    g = a & b;
+
+    s = p ^ cin;
+    //sReg = p ^ cin;
+    cout = g | (p & cin);
+    //coutReg = g | (p & cin);
+
+  end
+
+   //assign s = sReg;
+   //assign cout = coutReg;
+
+endmodule
+*/
